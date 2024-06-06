801f184d7130f22e2f9d229c9ef8d30e682b9b38
0gonge <Challengef0802@gmail.com>
2024-06-06 22:31:46 +0900

    actionBotton, Extention구현 - constraints 공부하고 이해하고 익숙해지기

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -10,17 +10,29 @@ import UIKit
 class MainTabController: UITabBarController {
     
     //MARK: - Properties
+    let actionButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        button.backgroundColor = .blue
+        button.setImage(UIImage(named: "new_tweet"), for: .normal)
+        return button
+    }()
     
     //MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         configureViewControllers()
+        configureUI()
     }
     
     //MARK: - Helpers
+    
+    func configureUI(){
+        view.addSubview(actionButton) //safeArea는 기종이 다를때에도 대응해줌. 
+        actionButton.anchor(bottom: view.safeAreaLayoutGuide.bottomAnchor, right: view.rightAnchor, paddingBottom: 64, paddingRight: 16, width: 56, height: 56)
+        actionButton.layer.cornerRadius = 56 / 2 //그러면 원이 될 것임 넓이 높이 나누기 2
+    }
     func configureViewControllers() {
         let feed = FeedController()
         let nav1 = templateNavigationController(image: UIImage(named: "home_unselected"), rootViewController: feed)
@@ -48,7 +60,7 @@ class MainTabController: UITabBarController {
         let appearance = UINavigationBarAppearance()
         appearance.configureWithOpaqueBackground()
         appearance.backgroundColor = .white
-        appearance.shadowColor = .gray //이렇게 해주니까 아래에 segment효과를 줄 수 있었다.! 
+        appearance.shadowColor = .gray //이렇게 해주니까 아래에 segment효과를 줄 수 있었다
         
         nav.navigationBar.standardAppearance = appearance
         nav.navigationBar.scrollEdgeAppearance = appearance
