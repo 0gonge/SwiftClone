8117eb1271a04e5e2c8024fc1822632c1faaf3d7
0gonge <Challengef0802@gmail.com>
2024-06-06 19:49:53 +0900

    TabBarController Basic Setting

---
+++ TwitterClone/Controller/MainTabController.swift
@@ -0,0 +1,18 @@
+//
+//  MainTabController.swift
+//  TwitterClone
+//
+//  Created by 송여경 on 6/6/24.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemBlue
+    }
+
+}
---
+++ TwitterCloneUITests/TwitterCloneUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TwitterCloneUITestsLaunchTests.swift
+//  TwitterCloneUITests
+//
+//  Created by 송여경 on 6/6/24.
+//
+
+import XCTest
+
+final class TwitterCloneUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
