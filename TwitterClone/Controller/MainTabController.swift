a871707f84e9f114cda9520cf5e1f8f7b87179aa
0gonge <Challengef0802@gmail.com>
2024-06-06 20:19:17 +0900

    TabBarController with segment 이미지

--- TwitterClone/Controller/MainTabController.swift
+++ TwitterClone/Controller/MainTabController.swift
@@ -22,10 +22,17 @@ class MainTabController: UITabBarController {
     //MARK: - Helpers
     func configureViewControllers(){ //programatically하게 다른 모든 컨트롤러를 구현해줄 곳이다.
         let feed = FeedController()
-        let explore = ExploreController()
-        let notifications = NotificationsController()
-        let conversations = ConversationsController()
+        feed.tabBarItem.image = UIImage(named: "home_unselected")
         
+        let explore = ExploreController()
+        explore.tabBarItem.image = UIImage(named: "search_unselected")
+        
+        let notifications = NotificationsController()
+        notifications.tabBarItem.image = UIImage(named: "like_unselected")
+
+        let conversations = ConversationsController()
+        conversations.tabBarItem.image = UIImage(named: "search_unselected")
+
         viewControllers = [feed, explore, notifications, conversations]
         //필요한 보기 컨트롤러 속성들을 array로 정의를 해주었다.
         //UITabBarController의 sub나 subclass로 가져올 것.
