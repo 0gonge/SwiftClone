cee91b2bf1311635ee02516b961ad7f44211a1d1
0gonge <Challengef0802@gmail.com>
2024-07-03 04:49:28 +0900

    [FEAT] TextView StackView로 구현, selector 구현

--- TwitterClone/Controller/UploadTweetController.swift
+++ TwitterClone/Controller/UploadTweetController.swift
@@ -74,8 +74,12 @@ class UploadTweetController: UIViewController {
     view.backgroundColor = .white
     configureNavigationBar()
     
-    view.addSubview(profileImageView)
-    profileImageView.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, paddingTop: 16, paddingLeft: 16)
+    let stack = UIStackView(arrangedSubviews: [profileImageView, captionTextView])
+    stack.axis = .horizontal
+    stack.spacing = 12
+    
+    view.addSubview(stack)
+    stack.anchor(top: view.safeAreaLayoutGuide.topAnchor, left: view.leftAnchor, right: view.rightAnchor, paddingTop: 16, paddingLeft: 16, paddingRight: 16)
     
     profileImageView.sd_setImage(with: user.profileImageUrl, completed: nil)
     
--- TwitterClone/View/CaptionTextView.swift
+++ TwitterClone/View/CaptionTextView.swift
@@ -15,6 +15,7 @@ class CaptionTextView: UITextView {
     let label = UILabel()
     label.font = UIFont.systemFont(ofSize: 16)
     label.textColor = .darkGray
+    label.text = "What's happening?"
     return label
   }()
   
@@ -30,12 +31,25 @@ class CaptionTextView: UITextView {
     //텍스트 뷰의 높이를 300 포인트로 고정하는 제약 조건
     //view.을 안해주는 이유는 이미 UITextView가 UIview의 subclass이기 때문이다.
     addSubview(placeholderLabel)
-    placeholderLabel.anchor(top: topAnchor, left: leftAnchor,paddingTop: 8, paddingLeft: 4)
+    placeholderLabel.anchor(top: topAnchor, left: leftAnchor, paddingTop: 8, paddingLeft: 4)
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(handleTextInputChange), name: UITextView.textDidChangeNotification, object: nil)
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
   
-  
+  //MARK: - Selectors
+  @objc func handleTextInputChange(){
+    placeholderLabel.isHidden = !text.isEmpty
+    //if문과 비슷한 역할을 해 준다. Empty가 아닐때, 그러니까 무언가 써져있을 때 placeholderLabel을 숨겨준다.
+    //Empty가 true면 ! 하면 false / hidden 하지 않는다. 직관적 이해 완.
+    //if text.isEmpty {
+    //    placeholderLabel.isHidden = false
+    //  }
+    //  else {
+    //    placeholderLabel.isHidden = true
+    //  }
+  }
 }
