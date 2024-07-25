7afa6b0e3f23cde92d9b8302d1ec98ddde7eef81
0gonge <Challengef0802@gmail.com>
2024-07-26 05:50:53 +0900

    [Feat] TimeStamp 라벨에 표시하기

--- TwitterClone/ViewModel/TweetViewModel.swift
+++ TwitterClone/ViewModel/TweetViewModel.swift
@@ -18,11 +18,48 @@ struct TweetViewModel {
     return user.profileImageUrl
   }
   
+  var timestamp: String {
+    let formatter = DateComponentsFormatter()
+    formatter.allowedUnits = [.second, .minute, .hour, .day, .weekOfMonth]
+    formatter.maximumUnitCount = 1
+    //가장 큰 단위 하나만 표시해주기 위함이다.
+    formatter.unitsStyle = .abbreviated
+    //축약된 형태로 표현
+    let now = Date()
+    return formatter.string(from: tweet.timestamp, to: now) ?? "2m"
+    //timestamp부터 지금까지의 시간의 차이를 반환해주는 것임. (상대적 표시)
+  }
+  
   var userInfoText: NSAttributedString {
-    let title = NSMutableAttributedString(string: user.fullname,
-                                          attributes: [.font: UIFont.boldSystemFont(ofSize: 14)])
-    title.append(NSAttributedString(string: " @\(user.username)", attributes: [.font: UIFont.systemFont(ofSize: 14),
-                                                                               .foregroundColor:UIColor.lightGray]))
+    let title = NSMutableAttributedString(
+      string: user.fullname,
+      attributes: [.font: UIFont.boldSystemFont(
+        ofSize: 14
+      )]
+    )
+    title.append(
+      NSAttributedString(
+        string: " @\(user.username)",
+        attributes: [
+          .font: UIFont.systemFont(
+            ofSize: 14
+          ),
+          .foregroundColor:UIColor.lightGray
+        ]
+      )
+    )
+    title.append(
+      NSAttributedString(
+        string: " ・ \(timestamp)",
+        attributes: [
+          .font: UIFont.systemFont(
+            ofSize: 14
+          ),
+          .foregroundColor: UIColor.lightGray
+        ]
+      )
+    )
+    
     return title
   }
   
