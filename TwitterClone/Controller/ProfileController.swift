949a8f5ee5f51ee749893d79c7805686dd60b5cb
0gonge <Challengef0802@gmail.com>
2024-07-26 07:09:09 +0900

    [Feat] ProfileImageTapped Delegate설정

---
+++ TwitterClone/Controller/ProfileController.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileController.swift
+//  TwitterClone
+//
+//  Created by 송여경 on 7/26/24.
+//
+
+import UIKit
+
+class ProfileController: UICollectionViewController {
+  //MARK: - Properties
+  
+  //MARK: - Lifecycle
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    view.backgroundColor = .yellow
+  }
+}
